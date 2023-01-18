af0ae14b49d6243a5a7ead5f122352064c26e804
jusa-a <jusa.annevirta@aalto.fi>
2023-01-18 10:34:02 +0200

    add link to github, improve view for waiting data

--- client/src/App.js
+++ client/src/App.js
@@ -14,54 +14,59 @@ const App = () => {
         return () => clearInterval(interval)
     }, [])
 
-    if (drones[0]) {
-        return (
-            <div className='App'>
-                <h1>NDZ violations in last 10 min</h1>
-                <p>(active violations highlighted in red)</p>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Phone</th>
-                            <th>Email</th>
-                            <th>Closest Distance</th>
-                            <th>Last Detected</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {drones.map((drone) => (
-                            <tr
-                                key={drone.serialNumber}
-                                style={{
-                                    backgroundColor: drone.violating
-                                        ? '#f48989'
-                                        : '',
-                                }}
-                            >
-                                <td>
-                                    {drone.pilot.firstName}{' '}
-                                    {drone.pilot.lastName}
-                                </td>
-                                <td>{drone.pilot.phoneNumber}</td>
-                                <td>{drone.pilot.email}</td>
-                                <td>{drone.distance.toFixed(2)} m</td>
-                                <td>
-                                    {new Date(drone.timestamp).toLocaleString(
-                                        'en-GB',
-                                        {
-                                            timeZone: 'Europe/Helsinki',
-                                        }
-                                    )}
-                                </td>
+    return (
+        <div className='App'>
+            <h1>NDZ violations in last 10 min</h1>
+            <p>(active violations highlighted in red)</p>
+            <a href='https://github.com/jusa-a/reaktor-birdnest'>
+                <p>link to GitHub</p>
+            </a>
+            <div>
+                {!drones[0] ? (
+                    <h2>No drone data yet, please wait...</h2>
+                ) : (
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Phone</th>
+                                <th>Email</th>
+                                <th>Closest Distance</th>
+                                <th>Last Detected</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            {drones.map((drone) => (
+                                <tr
+                                    key={drone.serialNumber}
+                                    style={{
+                                        backgroundColor: drone.violating
+                                            ? '#f48989'
+                                            : '',
+                                    }}
+                                >
+                                    <td>
+                                        {drone.pilot.firstName}{' '}
+                                        {drone.pilot.lastName}
+                                    </td>
+                                    <td>{drone.pilot.phoneNumber}</td>
+                                    <td>{drone.pilot.email}</td>
+                                    <td>{drone.distance.toFixed(2)} m</td>
+                                    <td>
+                                        {new Date(
+                                            drone.timestamp
+                                        ).toLocaleString('en-GB', {
+                                            timeZone: 'Europe/Helsinki',
+                                        })}
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                )}
             </div>
-        )
-    }
-    return <div>No drone data, please wait...</div>
+        </div>
+    )
 }
 
 export default App
